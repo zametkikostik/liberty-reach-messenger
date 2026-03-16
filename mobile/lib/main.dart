8bca26a7a9e554ce0e1cc9ff90e733bf9ebda04b
zametkikostik <zametkikostik@gmail.com>
2026-03-16 12:23:34 +0300

    ⚙️ System: Migration to Flutter V2 Android Embedding completed

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,26 +1,25 @@
-name: liberty_reach_messenger
-description: "A secure, decentralized messenger - Liberty Reach."
-publish_to: 'none'
-version: 0.4.0+1
+import 'package:flutter/material.dart';
 
-environment:
-  sdk: '>=3.0.0 <4.0.0'
+void main() {
+  runApp(const LibertyReachApp());
+}
 
-dependencies:
-  flutter:
-    sdk: flutter
-  cupertino_icons: ^1.0.6
-  
-  # Пакеты для работы ApiService
-  http: ^1.1.0
-  flutter_secure_storage: ^9.0.0
+class LibertyReachApp extends StatelessWidget {
+  const LibertyReachApp({super.key});
 
-dev_dependencies:
-  flutter_test:
-    sdk: flutter
-  flutter_lints: ^3.0.0
-
-flutter:
-  uses-material-design: true
-
-  # Если у тебя будут свои иконки или шрифты, добавим их ниже
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Liberty Reach',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Liberty Reach - Coming Soon'),
+        ),
+      ),
+    );
+  }
+}
